4ca9db4ee8191702f14d53d86a5908cc61abb746
Farrukh Shahzad <farrukhshahzad2002@hotmail.com>
2020-03-27 10:05:20 -0500

    Added Test, fix kr20

--- kr20.py
+++ kr20.py
@@ -1,51 +1,36 @@
 import numpy as np 
-import json
-
-from flask import Flask, escape, request
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
 
 
-@app.route('/')
-def welcome():
-    return {'message': 'Hello'}
+def get_list(item, index):
 
-
-@app.route('/kr20/<json_array>', methods=['POST', 'GET'])
-def compute_kr20(json_array):
-    req = request.get_json(json_array)
-    return calculate_kr20(req)
+    return list(item[index].values())[0]
 
 
 def calculate_kr20(param):
     student_list = list(param['students'])
     numStudents = len(student_list)
-    numQ = len(student_list[0].values())
+    numQ = len(get_list(student_list, 0))
     pqList = []
     scoreList = []
 
     for k in range(0, numStudents):
-        if (numQ != len(student_list[k].values())):
+        if numQ != len(get_list(student_list, k)):
             return {'Error': 'All student\'s item count must be the same'}
 
     for i in range(0, numQ):
         p = 0
         for k in range(0, numStudents):
-            p += student_list[k].values()[i]
+            p += get_list(student_list, k)[i]
         p /= numStudents
         q = 1 - p
         pqList.append(p * q)
     pqSum = sum(pqList)
 
     for k in range(0, numStudents):
-        score = sum(student_list[k].values())
+        score = sum(get_list(student_list, k))
         scoreList.append(score)
+
     scoreSTD = np.std(scoreList)
-
     kr20_value = (numQ /(numQ - 1)) * (1 - (pqSum / (scoreSTD ** 2)))
-    return {'kr20': round(kr20_value, 3)}
 
-
-app.run()
+    return {'KR20': round(kr20_value, 3)}
