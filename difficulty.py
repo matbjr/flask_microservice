7a58c329b0258cd97f3e844f09fc4d7b7b109118
A-Baji <bidabaji@gmail.com>
2020-03-29 18:43:56 -0500

    Updated difficulty.py
    Made calculation simpler

--- difficulty.py
+++ difficulty.py
@@ -12,8 +12,8 @@ def calculate_difficulty(param):
     for i in range(0, numQ): # For each question i
         numRight = 0
         for k in range(0, numStudents): # For each student k
-            if get_list(student_list, k)[i] == 1: # If student k gets question i correct
-                numRight += 1
+            studentAnswer = get_list(student_list, k)[i]
+            numRight += studentAnswer
         difficulty = numRight / numStudents
         difficulty = round(difficulty, 3)
         difficultyList.append(difficulty)
