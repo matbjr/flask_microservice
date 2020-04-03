1fb86c2f108018d1868fd14694ba7337a32bcd5a
A-Baji <bidabaji@gmail.com>
2020-04-02 21:23:04 -0500

    pbcc updates for json changes
    pbcc.py now uses a dictionary to sort student responses.
    
    Notes:
    - Issues using imported get_list from utils.py
    - rightMean = wrongMean = None <-- Causing errors

--- pbcc.py
+++ pbcc.py
@@ -2,44 +2,75 @@
 from statistics import mean
 from statistics import pstdev
 from math import sqrt
-from utils import get_list
+# from utils import get_list
+
+# Having issues using imported get_list from utils.py
+def get_list(item, index):
+    return list(item[index]['itemresponses'])
 
 
 def calculate_pbcc(param):
-    student_list = list(param['students'])
+    student_list = list(param['studentList'])
+    responseList = get_list(student_list,0)
     numStudents = len(student_list)
-    numQ = len(get_list(student_list, 0))
+    numItems = len(responseList)
+    idList = []
+    responses = {}
     scoreList = []
     pbccList = []
 
+    for i in range(0, numItems): # Put all item IDs into a list
+        idList.append(responseList[i]['itemid'])
+
+    idList.sort()
+    for i in idList: # Create a dictionary with the item IDs as keys
+        responses[i] = []
+    
+    for i in range(0, numStudents): # For each student i
+        for k in range(0, numItems): # For each question k
+            for j in responses: # For each item ID j
+                if get_list(student_list, i)[k]['itemid'] == j: # If item IDs match, add response to dictionary
+                    responses[j].append(get_list(student_list, i)[k]['response'])
+
+    sortedResponses = []
+    for i in range(0, numStudents): # For each student
+        studentResponses = []
+        for k in responses: # For every item ID
+            studentResponses.append(responses[k][i]) # Create a list of the students responses sorted by item ID
+        sortedResponses.append(studentResponses)
+
     for i in range(0, numStudents): # Check if item count is consistent
-        if numQ != len(get_list(student_list, i)):
+        if numItems != len(sortedResponses[i]):
             return {'Error': 'All students\' item count must be the same'}
 
     for i in range(0, numStudents): # Get standard deviation of scores
-        score = sum(get_list(student_list, i))
+        score = sum(sortedResponses[i])
         scoreList.append(score)
     scoreSTD = pstdev(scoreList)
 
-    for i in range(0, numQ): # For each question i
+    for i in range(0, numItems): # For each question i
         rightList = [] # Scores of students who got question i right
         wrongList = [] # Scores of students who got question i wrong
         numRight = 0 # Total number of students who got question i right
         numWrong = 0 # Total number of students who got question i wrong
         for k in range(0, numStudents): # For each student k
-            if get_list(student_list, k)[i] == 1: # If student k gets question i correct
-                score = sum(get_list(student_list, k)) / numQ
+            if sortedResponses[k][i] == 1: # If student k gets question i correct
+                score = sum(sortedResponses[k]) / numItems
                 rightList.append(score) # Then add their score to the "right" list
                 numRight += 1
-            elif get_list(student_list, k)[i] == 0: # If student k gets question i wrong 
-                score = sum(get_list(student_list, k)) / numQ
+            elif sortedResponses[k][i] == 0: # If student k gets question i wrong 
+                score = sum(sortedResponses[k]) / numItems
                 wrongList.append(score) # Then add their score to the "wrong" list
                 numWrong += 1
 
-        rightMean = wrongMean = None
-        if len(rightList) > 0:
+        # rightMean = wrongMean = None <-- Causing errors
+        if len(rightList) == 1:
+            rightMean = rightList[0]
+        elif len(rightList) > 1:
             rightMean = mean(rightList)
-        if len(wrongList) > 0:
+        if len(wrongList) == 1:
+            wrongMean = wrongList[0]
+        elif len(wrongList) > 1:
             wrongMean = mean(wrongList)
         if not rightMean or not wrongMean:
             return {'pbcc': 'Invalid Data - No mean'}
@@ -47,5 +78,10 @@ def calculate_pbcc(param):
         pbcc = ((rightMean - wrongMean) * sqrt(numRight * numWrong)) / numStudents * scoreSTD
         pbcc = round(pbcc, 3)
         pbccList.append(pbcc)
+
+    k = 0
+    for i in idList:
+        responses[i] = pbccList[k]
+        k += 1
         
-    return {'pbcc': pbccList}
+    return {'pbcc': responses}
