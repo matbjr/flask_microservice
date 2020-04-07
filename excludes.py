3317f45703f5b2c0a63e6ce58c0918620e828fb5
A-Baji <bidabaji@gmail.com>
2020-04-07 17:31:53 -0500

    Update excludes.py

--- excludes.py
+++ excludes.py
@@ -7,7 +7,16 @@ def get_exclude_recos(param):
     idr_dict = list(calculate_idr(param).values())[0]
     exclude_list = []
     for i in idr_dict:
-        if idr_dict[i] <= 0.09:
+        if idr_dict[i] <= 0.09: # Hard coded
             exclude_list.append(i)
+    
+    if len(exclude_list) >= len(idr_dict)*0.5: # Hard coded
+        exclude_list = []
+        for i in idr_dict:
+            if idr_dict[i] < 0: # Hard coded
+                exclude_list.append(i)
+    
+    # if len(exclude_list) >= len(idr_dict)*0.8:
+    #     then suggest that this may be a poor exam with very low exam reliability.
         
     return {service_key: exclude_list}
