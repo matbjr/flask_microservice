1e26e4458527f5dbc4f4ca6baa8f1581ae6589ce
Farrukh Shahzad <farrukhshahzad2002@hotmail.com>
2020-04-07 15:41:41 -0500

    Jsonify results, Sample added, fix tests

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, jsonify
+from flask_cors import cross_origin
 import json
+from utils import get_config
+from sample import sample
 
 from std import calculate_std
 from summation import calculate_summation
@@ -18,7 +20,6 @@ from excludes import get_excludes
 
 app = Flask(__name__)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
-CORS(app)
 
 
 def process_request(json_data: str, fn):
@@ -34,16 +35,18 @@ def process_request(json_data: str, fn):
     try:
         inp = json.loads(json_data)
         ans = fn(inp)  # calling function 'fn'
-        ans['input'] = inp
+        ans['Input'] = inp
     except Exception as exc:
         ans = {'error': str(exc), 'input': json_data}
 
-    return json.dumps(ans)
+    return jsonify(ans)
 
 
 @app.route('/')
+@cross_origin()
 def welcome():
-    return json.dumps({'message': 'Welcome from Reliability Measures!'})
+    return jsonify({'message': 'Welcome from Reliability Measures!',
+                    'version': get_config('application_version')})
 
 
 @app.route('/std/<json_data>', methods=['POST', 'GET'])
@@ -87,6 +90,7 @@ def compute_average(json_data):
 
 
 @app.route('/analyzeTest/<json_data>', methods=['POST', 'GET'])
+@cross_origin()
 def get_analysis(json_data):
     return process_request(json_data, analyze_test_scores)
 
@@ -106,5 +110,13 @@ def compute_excludes(json_data):
     return process_request(json_data, get_excludes)
 
 
+@app.route('/sample', methods=['POST', 'GET'])
+@cross_origin()
+def get_sample_analysis():
+    return process_request(json.dumps(sample), analyze_test_scores)
+
+
 if __name__ == '__main__':
-    app.run(threaded=True)
+    print("Starting service")
+    app.run(host="0.0.0.0", port=8000, threaded=True)
+
--- test_functions.py
+++ test_functions.py
@@ -7,6 +7,7 @@ from analyze_test import analyze_test_scores
 from weighted_scores import calculate_weighted_scores
 from weighted_average import calculate_weighted_average
 from excludes import get_excludes
+from utils import get_service_config
 
 
 class TestFunctions:
@@ -16,13 +17,13 @@ class TestFunctions:
     # self.data is a class variable that can be used through out the class
     def setup(self):
         self.data = {
-            "exam_info":
+            "exam":
             { 
                 "name": "test1"
             },
             "student_list": [
                 { 
-                    "grad_year": "2022",
+                  "grad_year": "2022",
                   "id": 1234,
                   "first_name": "John",
                   "last_name": "Smith",
@@ -69,12 +70,18 @@ class TestFunctions:
 
     # testing analyze_test
     def test_analyze(self):
-        expected = [{'kr20': 0.343}, {
-            'pbcc': {1: 0.049, 2: -0.049, 3: 0.245, 4: 0.245, 5: 0.196,
-                     6: 0.0}}, {
-                        'difficulty': {1: 0.333, 2: 0.667, 3: 0.667, 4: 0.667,
-                                       5: 0.333, 6: 1.0}},
-                    {'scores': [0.667, 0.833, 0.333]}, {'average': 0.611}]
+        expected = [
+            {get_service_config(1): 0.343},
+            {get_service_config(2): {1: 0.049, 2: -0.049, 3: 0.245, 4: 0.245, 5: 0.196,
+                      6: 0.0}},
+            {get_service_config(3): {1: 0.333, 2: 0.667, 3: 0.667, 4: 0.667,
+                            5: 0.333, 6: 1.0}},
+            {get_service_config(4): [0.667, 0.833, 0.333]},
+            {get_service_config(5): 0.611},
+            {get_service_config(7): [0.727, 0.909, 0.455]},
+            {get_service_config(8): 0.697},
+            {get_service_config(9): [2]}
+        ]
 
         analysis = analyze_test_scores(self.data)['analysis']
 
@@ -84,14 +91,14 @@ class TestFunctions:
     def test_kr20(self):
 
         expected = 0.343
-        kr20 = calculate_kr20(self.data)['kr20']
+        kr20 = calculate_kr20(self.data)[get_service_config(1)]
 
         assert kr20 == expected
 
     # testing the pbcc
     def test_pbcc(self):
         expected = {1: 0.049, 2: -0.049, 3: 0.245, 4: 0.245, 5: 0.196, 6: 0.0}
-        pbcc = calculate_pbcc(self.data)['pbcc']
+        pbcc = calculate_pbcc(self.data)[get_service_config(2)]
 
         assert pbcc == expected
 
@@ -99,7 +106,7 @@ class TestFunctions:
     def test_difficulty(self):
 
         expected = {1: 0.333, 2: 0.667, 3: 0.667, 4: 0.667, 5: 0.333, 6: 1.0}
-        difficulty = calculate_difficulty(self.data)['difficulty']
+        difficulty = calculate_difficulty(self.data)[get_service_config(3)]
 
         assert difficulty == expected
 
@@ -107,7 +114,7 @@ class TestFunctions:
     def test_scores(self):
 
         expected = [0.667, 0.833, 0.333]
-        scores = calculate_scores(self.data)['scores']
+        scores = calculate_scores(self.data)[get_service_config(4)]
 
         assert scores == expected
 
@@ -115,7 +122,7 @@ class TestFunctions:
     def test_average(self):
 
         expected = 0.611
-        average = calculate_average(self.data)['average']
+        average = calculate_average(self.data)[get_service_config(5)]
 
         assert average == expected
 
@@ -123,7 +130,7 @@ class TestFunctions:
     def test_weighted_scores(self):
 
         expected = [0.727, 0.909, 0.455]
-        scores = calculate_weighted_scores(self.data)['weighted_s']
+        scores = calculate_weighted_scores(self.data)[get_service_config(7)]
 
         assert scores == expected
 
@@ -131,7 +138,7 @@ class TestFunctions:
     def test_weighted_avg(self):
 
         expected = 0.697
-        average = calculate_weighted_average(self.data)['weighted_avg']
+        average = calculate_weighted_average(self.data)[get_service_config(8)]
 
         assert average == expected
 
@@ -139,6 +146,6 @@ class TestFunctions:
     def test_get_excludes(self):
 
         expected = [2]
-        average = get_excludes(self.data)['exclude']
+        average = get_excludes(self.data)[get_service_config(9)]
 
         assert average == expected
